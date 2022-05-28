dc986bd177a3cc0dbcf0d32cf66deb3405208d10
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-05-28 22:27:39 +0600

    first add

---
+++ src/main/java/com/example/project/global/GlobalData.java
@@ -0,0 +1,13 @@
+package com.example.project.global;
+
+import com.example.project.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlobalData {
+    public static List<Product> cart;
+    static {
+        cart = new ArrayList<Product>();
+    }
+}
